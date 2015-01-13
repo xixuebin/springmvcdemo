2c83b5714dcff7dc27165823bcec14d9ef7e934f
xixuebin <xixuebin@163.com>
2015-01-13 18:09:10 +0800

    add log file

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -1,5 +1,7 @@
 package com.springapp.mvc;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class HelloController {
 
+	private   static Log log = LogFactory.getLog(HelloController.class);
+
 	@RequestMapping(params = "testspring", method = RequestMethod.GET)
 	public String testSpring(ModelMap model){
-		System.out.println("test spring");
+		log.info("test spring");
 		model.addAttribute("testspring", "this is a test for spring");
 		return "springTest";
 	}
@@ -19,7 +23,7 @@ public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		System.out.println("print Welcome");
+		log.info("print Welcome");
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
